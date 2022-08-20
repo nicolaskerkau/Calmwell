40ab59a84e83982d8b01adec6da22c1fa1103a1e
Nicolas Kerkau <nicolas.a.kerkau@gmail.com>
2022-08-20 23:45:01 +0300

    Initial Commit

---
+++ Calmwell/CalmwellApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalmwellApp.swift
+//  Calmwell
+//
+//  Created by Nicolas A. Kerkau on 8/20/22.
+//
+
+import SwiftUI
+
+@main
+struct CalmwellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calmwell/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calmwell
+//
+//  Created by Nicolas A. Kerkau on 8/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
